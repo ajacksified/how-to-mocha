a5223aa001fc4cb87f55859907333cd9d6dedcd6
Stephen Wyatt Bush <stephen.bush@airbnb.com>
2013-12-12 14:26:15 -0800

    define get

--- src/models/character.js
+++ src/models/character.js
@@ -1,8 +1,9 @@
 Character = function(){
-
+  this.attributes = {};
 };
 
-Character.prototype.get = function() {
+Character.prototype.get = function(attr) {
+  return this.attributes[attr];
 };
 
 module.exports = Character;
