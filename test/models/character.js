531bc76408b1b8003ed1fc21f825b6d24826ecd8
Jack Lawson <ajacksified@gmail.com>
2013-12-12 14:17:31 -0800

    [character] expect get to exist

--- test/models/character.js
+++ test/models/character.js
@@ -7,4 +7,15 @@ describe("Character", function(){
   it("exists", function(){
     expect(Character).to.exist;
   });
+
+  it("it is a constructor", function(){
+    expect(typeof Character).to.equal('function');
+  });
+
+  describe("getting properties", function(){
+    it("has a get function", function(){
+      expect(Character.prototype.get).to.exist;
+      expect(typeof Character.prototype.get).to.equal('function');
+    });
+  });
 });
